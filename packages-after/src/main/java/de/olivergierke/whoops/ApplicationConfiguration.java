0a97dcd63556a2f1bb4acab4f23f92988cfa7bf0
Oliver Gierke <info@olivergierke.de>
2012-10-16 00:38:10 -0400

    Upgraded to Spring 3.2.0.M2.
    Simplified component scanning configuration.

--- packages-after/src/main/java/de/olivergierke/whoops/ApplicationConfiguration.java
+++ packages-after/src/main/java/de/olivergierke/whoops/ApplicationConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 the original author or authors.
+ * Copyright 2011-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,11 +23,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
 /**
- * 
  * @author Oliver Gierke
  */
 @Configuration
-@ComponentScan(basePackageClasses = ApplicationConfiguration.class, excludeFilters = @Filter(Controller.class))
+@ComponentScan(excludeFilters = @Filter(Controller.class))
 public class ApplicationConfiguration {
 
 	public static <T> Answer<T> withArgument() {
--- plugins-sample/options/src/main/java/de/olivergierke/whoops/hera/options/OptionConfig.java
+++ plugins-sample/options/src/main/java/de/olivergierke/whoops/hera/options/OptionConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 the original author or authors.
+ * Copyright 2011-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,7 +24,7 @@ import org.springframework.context.annotation.Configuration;
  * @author Oliver Gierke
  */
 @Configuration
-@ComponentScan(basePackageClasses = OptionConfig.class)
+@ComponentScan
 public class OptionConfig {
 
 }
